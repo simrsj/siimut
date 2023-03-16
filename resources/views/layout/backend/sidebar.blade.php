01d1db59be537e4e0dcbdf6772deeb581d5c131e
Regipurnama <rizkiegipurnama@gmail.com>
2023-03-16 13:07:21 +0700

    data indikator

--- resources/views/layout/backend/sidebar.blade.php
+++ resources/views/layout/backend/sidebar.blade.php
@@ -44,6 +44,20 @@
 
     @can('admin')
     <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#Indikator"
+            aria-expanded="true" aria-controls="collapseOne">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Indikator</span>
+        </a>
+        <div id="Indikator" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{ route('user.index') }}">Prioritas</a>
+                <a class="collapse-item" href="{{ route('user.index') }}">Corporate</a>
+                <a class="collapse-item" href="{{ route('user.index') }}">Nasional</a>
+            </div>
+        </div>
+    </li>
+    <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne"
             aria-expanded="true" aria-controls="collapseOne">
             <i class="fas fa-fw fa-table"></i>
